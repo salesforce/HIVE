abff43ce2aec91d10f8d5f1c5c01bee9c8027b5b
Shu Zhang <shu.zhang@shuzhan-ltmndgx.internal.salesforce.com>
2023-04-07 09:43:22 -0700

    update checkpoint downloading

--- scripts/download_checkpoints.sh
+++ scripts/download_checkpoints.sh
@@ -5,6 +5,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 mkdir -p $SCRIPT_DIR/../checkpoints
 
 curl https://huggingface.co/stabilityai/stable-diffusion-2-1-base/resolve/main/v2-1_512-ema-pruned.ckpt -o $SCRIPT_DIR/../checkpoints/v2-1_512-ema-pruned.ckpt
-curl https://storage.cloud.google.com/sfr-hive-data-research/checkpoints/hive_rw_condition.ckpt -o $SCRIPT_DIR/../checkpoints/hive_rw_condition.ckpt
-curl https://storage.cloud.google.com/sfr-hive-data-research/checkpoints/hive_v2_rw_condition.ckpt -o $SCRIPT_DIR/../checkpoints/hive_v2_rw_condition.ckpt
-curl https://storage.cloud.google.com/sfr-hive-data-research/checkpoints/hive_rw.ckpt -o $SCRIPT_DIR/../checkpoints/hive_rw.ckpt
+gcloud storage cp gs://sfr-hive-data-research/checkpoints/hive_rw_condition.ckpt $SCRIPT_DIR/../checkpoints/hive_rw_condition.ckpt
+gcloud storage cp gs://sfr-hive-data-research/checkpoints/hive_v2_rw_condition.ckpt $SCRIPT_DIR/../checkpoints/hive_v2_rw_condition.ckpt
+gcloud storage cp gs://sfr-hive-data-research/checkpoints/hive_rw.ckpt $SCRIPT_DIR/../checkpoints/hive_rw.ckpt
